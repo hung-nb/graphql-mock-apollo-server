3f90aecab210625ccfb3042e19013a4ee91fe65c
Hung Nguyen <hung@redhotminute.com.au>
2019-12-02 12:31:18 +1100

    Add "mocks" function to overwrite "resolvers" function

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
   type Query {
     hello: String
+    resolved: String
   }
 `;
 
+// Resolvers tell Apollo Server how to fetch the data associated with a particular type
+const resolvers = {
+  Query: {
+    resolved: () => 'Resolved',
+  },
+};
+
+// Mock function which overwrites the resolvers
+const mocks = {
+  Int: () => 6,
+  Float: () => 22.1,
+  String: () => 'Hello',
+};
+
 const server = new ApolloServer({
   typeDefs,
-  mocks: true,
+  resolvers,
+  mocks,  // This overwrites resolvers
 });
 
 server.listen().then(({ url }) => {
